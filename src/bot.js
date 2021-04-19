46213eccf4c490f80fb985863ef27458b1252c9c
Arthur Melton <amtitan@icloud.com>
2021-04-19 16:51:08 -0400

    whatisnsfw

--- src/bot.js
+++ src/bot.js
@@ -50,6 +50,7 @@ client.on('message', (message) => {
 			{ name: `${Prefix}random`, value: "shows a random anime img" },
 			{ name: `${Prefix}wallpaper`, value: "shows a random anime wallpaper (just a random one from the first 25 hot on the subreddit)" },
 			{ name: `${Prefix}nsfw`, value: "shows random nsfw anime img (only in nsfw marked channels)" },
+			{ name: `${Prefix}whatisnsfw`, value: "runs nsfw then dose a whatis on it (gets a random hentai)" },
 		],
 		image: {
 			url: ""
@@ -266,6 +267,54 @@ client.on('message', (message) => {
 	  		})
   	  	}
   	  	else {
+  	  		message.channel.send("sorry but the channel is not marked as nsfw");
+  	  	}
+  	}
+  	if (commandName === 'whatisnsfw') {
+  		if (message.channel.nsfw === true || message.guild === null) {
+		    request('https://crunchy-bot.live/api/nsfw/hentai?tag={}', function (error, response, body) {
+			  	if (!error && response.statusCode == 200) {
+			  		var jsonParsed1 = JSON.parse(body);
+				    request(`https://trace.moe/api/search?url=${jsonParsed1.url}`, function (error, response, body) {
+					  	if (!error && response.statusCode == 200) {
+					  		var jsonParsed = JSON.parse(body);
+					  		if (jsonParsed.docs[0].season === '') {
+					  			jsonParsed.docs[0].season = '0';
+					  		}
+					    	const Embed = {
+								color: '#00ff00',
+								title: jsonParsed.docs[0].anime,
+								url: `https://duckduckgo.com/?q=${jsonParsed.docs[0].anime.replaceAll(" ", "+")}`,
+								author: {
+									Name: 'AnimeBot',
+									icon_url: jsonParsed1.url,
+									url: '',
+								},
+								description: `${Math.round((jsonParsed.docs[0].similarity) * 100)}% confident`,
+								thumbnail: jsonParsed1.url,
+								fields: [
+									{ name: 'Season', value: jsonParsed.docs[0].season, inline: true },
+									{ name: 'Episode', value: jsonParsed.docs[0].episode, inline: true },
+									{ name: 'hentai', value: jsonParsed.docs[0].is_adult, inline: true },
+									{ name: 'Image shown', value: `${Math.round(jsonParsed.docs[0].from)} secs in`, inline: true },
+								],
+								image: {
+									url: jsonParsed1.url,
+								},
+								fimestamp: new Date(),
+								footer: {
+									test: 'Some footer text here',
+									icon_url: jsonParsed1.url,
+								},
+							}
+
+							message.channel.send({ embed: Embed });
+					  	} 
+				  	})
+				}
+			})
+		}
+		else {
   	  		message.channel.send("sorry but the channel is not marked as nsfw");
   	  	}
   	}
