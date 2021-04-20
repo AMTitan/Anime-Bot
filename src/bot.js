b9f16b3a45b15204d0ed3bf197e139de37cd1ea8
Arthur Melton <amtitan@icloud.com>
2021-04-19 21:25:22 -0400

    working

--- src/bot.js
+++ src/bot.js
@@ -15,14 +15,6 @@ request(`https://animechan.vercel.app/api/random`, function (error, response, bo
   	}
 });
 */
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
 client.on('ready', () => {
   console.log(`${client.user.tag} bot is on`);
    client.user.setActivity(`${Prefix}help`, { type: 'WATCHING' })
