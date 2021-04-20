84426776127fdbb8cca2083b8fb16c0a3e912afd
Arthur Melton <amtitan@icloud.com>
2021-04-19 21:20:43 -0400

    init mysql

--- src/bot.js
+++ src/bot.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const { Client } = require('discord.js');
 const client = new Client();
 const Prefix = "A!";
-var mongo = require('mongodb');
 fs = require('fs');
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -16,6 +15,14 @@ request(`https://animechan.vercel.app/api/random`, function (error, response, bo
   	}
 });
 */
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
 client.on('ready', () => {
   console.log(`${client.user.tag} bot is on`);
    client.user.setActivity(`${Prefix}help`, { type: 'WATCHING' })
