ea7f8cd308cf3c72496394ef630d6c0250ace1bb
Arthur Melton <amtitan@icloud.com>
2021-04-20 08:46:53 -0400

    whatiswaifu

--- src/bot.js
+++ src/bot.js
@@ -51,6 +51,7 @@ client.on('message', (message) => {
 			{ name: `${Prefix}whatisnsfw`, value: "runs nsfw then dose a whatis on it (gets a random hentai)" },
 			{ name: `${Prefix}quotequestions`, value: "guess who said a quote" },
 			{ name: `${Prefix}waifu`, value: "gets you a waifu" },
+			{ name: `${Prefix}whatiswaifu`, value: "guess the waifu you get" },
 		],
 		image: {
 			url: ""
@@ -426,7 +427,131 @@ client.on('message', (message) => {
 				});
 
   		})
-  	}else if (commandName.toLowerCase() === 'waifu') {
+  	}else if (commandName.toLowerCase() === `whatiswaifu`) {
+    	const options = {
+		  method: 'GET',
+		  url: 'https://animu.p.rapidapi.com/waifus',
+		  headers: {
+		    'x-rapidapi-key': 'f51fa7a829msh002a66b9e9ebd76p1e3400jsn4c71c0617a59',
+		    'x-rapidapi-host': 'animu.p.rapidapi.com',
+		    useQueryString: true
+		  }
+		};
+
+		request(options, function (error, response, body) {
+			if (error) throw new Error(error);
+			var jsonParsed = JSON.parse(body);
+			const Embed = {
+				color: '#00ff00',
+				title: `Guess the waifu`,
+				url: "",
+				author: {
+					Name: 'AnimeBot',
+					icon_url: jsonParsed.images[0],
+					url: '',
+				},
+				description: ``,
+				thumbnail: args[0],
+				fields: [
+					{ name: 'From', value: jsonParsed.from.name, inline: true },
+					{ name: 'Type', value: jsonParsed.from.type, inline: true },
+				],
+				image: {
+					url: jsonParsed.images[0],
+				},
+				fimestamp: new Date(),
+				footer: {
+					test: 'Some footer text here',
+					icon_url: jsonParsed.images[0],
+				},
+			}
+
+			const filter = (n) => n.author.id === message.author.id;
+			message.channel.send({ embed: Embed });
+
+			message.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['Line']})
+				.then((collected) => {
+					const msg = collected.first().content;
+					if (msg.toLowerCase() === jsonParsed.names.en.toLowerCase()) {
+						const Embed = {
+							color: '#00ff00',
+							title: `You are right`,
+							url: "",
+							author: {
+								Name: 'AnimeBot',
+								icon_url: "",
+								url: '',
+							},
+							description: ``,
+							thumbnail: "",
+							fields: [
+							],
+							image: {
+								url: ""
+							},
+							fimestamp: new Date(),
+							footer: {
+								test: '',
+								icon_url: "",
+							},
+						}
+
+						message.channel.send({ embed: Embed });
+					}
+					else {
+						const Embed = {
+							color: '#00ff00',
+							title: `That is not right it was `+ jsonParsed.names.en,
+							url: "",
+							author: {
+								Name: 'AnimeBot',
+								icon_url: "",
+								url: '',
+							},
+							description: ``,
+							thumbnail: "",
+							fields: [
+							],
+							image: {
+								url: ""
+							},
+							fimestamp: new Date(),
+							footer: {
+								test: '',
+								icon_url: "",
+							},
+						}
+						message.channel.send({ embed: Embed });
+					}
+				})
+				.catch((err) =>  { 
+					const Embed = {
+						color: '#00ff00',
+						title: `You know you have to respond right? Welp it was `+ jsonParsed.names.en,
+						url: "",
+						author: {
+							Name: 'AnimeBot',
+							icon_url: "",
+							url: '',
+						},
+						description: ``,
+						thumbnail: "",
+						fields: [
+						],
+						image: {
+							url: ""
+						},
+						fimestamp: new Date(),
+						footer: {
+							test: '',
+							icon_url: "",
+						},
+					}
+					message.channel.send({ embed: Embed });
+				});
+
+	  		})
+	}else if (commandName.toLowerCase() === 'waifu') {
   		request(`https://api.slushie.gg/neko/waifu`, function (error, response, body) {
 		  	if (!error && response.statusCode == 200) {
 		  		var jsonParsed = JSON.parse(body);
