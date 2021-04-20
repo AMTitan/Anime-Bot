fb6e3660aeeeec0877c49f03c37be3380bd67237
Arthur Melton <amtitan@icloud.com>
2021-04-20 15:31:47 -0400

    now right titles

--- src/bot.js
+++ src/bot.js
@@ -199,7 +199,7 @@ client.on('message', (message) => {
 						var jsonParsed = JSON.parse(body);
 						const Embed = {
 							color: '#00ff00',
-							title: 'Neko',
+							title: 'Neko nsfw',
 							url: jsonParsed.url,
 							author: {
 								Name: 'AnimeBot',
@@ -233,7 +233,7 @@ client.on('message', (message) => {
 					var jsonParsed = JSON.parse(body);
 					const Embed = {
 						color: '#00ff00',
-						title: 'Neko nsfw',
+						title: 'Neko',
 						url: jsonParsed.url,
 						author: {
 							Name: 'AnimeBot',
