09356aeed9e35ea4996625d292fc01c7c8096d1f
Arthur Melton <amtitan@icloud.com>
2021-04-21 18:08:50 -0400

    stop cmd, autoneko, more

--- src/bot.js
+++ src/bot.js
@@ -52,6 +52,9 @@ client.on('message', (message) => {
 				name: `${Prefix}autoawoo (number of times to run nsfw - default 10) (delay in secs - defualt 5)`,
 				value: "dose awoo a set number of times"
 			},  {
+				name: `${Prefix}autoneko (number of times to run nsfw - default 10) (delay in secs - defualt 5)`,
+				value: "dose neko a set number of times"
+			},   {
 				name: `${Prefix}autonekonsfw (number of times to run nsfw - default 10) (delay in secs - defualt 5)`,
 				value: "dose neko nsfw a set number of times"
 			},  {
@@ -105,6 +108,9 @@ client.on('message', (message) => {
 			}, {
 				name: `${Prefix}show`,
 				value: "gets you a random "
+			}, {
+				name: `${Prefix}stop`,
+				value: "stops a auto anything but you have to be the msg author or a admin"
 			}, {
 				name: `${Prefix}waifu [optional nsfw]`,
 				value: "gets you a waifu"
@@ -841,8 +847,11 @@ client.on('message', (message) => {
 		if (!args[1]) args[1] = 5;
 		if (message.channel.nsfw === true || message.guild === null) {
 			var n = 0;
+			var run = true;
+			const filter = message.author.id;
 			for (i = 0; i < args[0]; i++) {
 				setTimeout(function(){
+					if (run === false) return;
 					n++;
 					request('https://crunchy-bot.live/api/nsfw/hentai?tag={}', function(error, response, body) {
 						if (!error && response.statusCode == 200) {
@@ -872,6 +881,37 @@ client.on('message', (message) => {
 							message.channel.send({
 								embed: Embed
 							});
+
+							client.on('message', (message1) => {
+								if ((filter === message1.author.id || message1.member.hasPermission("MANAGE_MESSAGES")) && message1.content.toLowerCase() === `${Prefix.toLowerCase()}stop`) {
+									run = false;
+								}else if(message1.author.Bot === false) {
+									const Embed = {
+										color: '#00ff00',
+										title: `Sorry you have to be the message author or the an admin`,
+										url: "",
+										author: {
+											Name: 'AnimeBot',
+											icon_url: "",
+											url: '',
+										},
+										description: ``,
+										thumbnail: "",
+										fields: [],
+										image: {
+											url: ""
+										},
+										fimestamp: new Date(),
+										footer: {
+											test: '',
+											icon_url: "",
+										},
+									}
+									message.channel.send({
+										embed: Embed
+									});
+								}
+							})
 						}
 					})
 					
@@ -885,8 +925,11 @@ client.on('message', (message) => {
 		if (!args[1]) args[1] = 5;
 		if (message.channel.nsfw === true || message.guild === null) {
 			var n = 0;
+			var run = true;
+			const filter = message.author.id;
 			for (i = 0; i < args[0]; i++) {
 				setTimeout(function(){
+					if (run === false) return;
 					n++;
 					request(`https://waifu.pics/api/nsfw/neko`, function(error, response, body) {
 					if (!error && response.statusCode == 200) {
@@ -916,6 +959,37 @@ client.on('message', (message) => {
 						message.channel.send({
 							embed: Embed
 						});
+
+						client.on('message', (message1) => {
+							if ((filter === message1.author.id || message.member1.hasPermission("MANAGE_MESSAGES")) && message1.content.toLowerCase() === `${Prefix.toLowerCase()}stop`) {
+								run = false;
+							}else if(message1.author.Bot === false) {
+								const Embed = {
+									color: '#00ff00',
+									title: `Sorry you have to be the message author or the an admin`,
+									url: "",
+									author: {
+										Name: 'AnimeBot',
+										icon_url: "",
+										url: '',
+									},
+									description: ``,
+									thumbnail: "",
+									fields: [],
+									image: {
+										url: ""
+									},
+									fimestamp: new Date(),
+									footer: {
+										test: '',
+										icon_url: "",
+									},
+								}
+								message.channel.send({
+									embed: Embed
+								});
+							}
+						})
 					}
 				})
 					
@@ -924,12 +998,89 @@ client.on('message', (message) => {
 		} else {
 			message.channel.send("sorry but the channel is not marked as nsfw");
 		}
+	} else if (commandName.toLowerCase() === 'autoneko') {
+		if (!args[0]) args[0] = 3;
+		if (!args[1]) args[1] = 5;
+		var n = 0;
+		var run = true;
+		const filter = message.author.id;
+		for (i = 0; i < args[0]; i++) {
+			setTimeout(function(){
+				if (run === false) return;
+				n++;
+				request(`https://waifu.pics/api/sfw/neko`, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					var jsonParsed = JSON.parse(body);
+					const Embed = {
+						color: '#00ff00',
+						title: `Neko ${n}/${args[0]}`,
+						url: jsonParsed.url,
+						author: {
+							Name: 'AnimeBot',
+							icon_url: jsonParsed.url,
+							url: '',
+						},
+						description: ``,
+						thumbnail: jsonParsed.url,
+						fields: [],
+						image: {
+							url: jsonParsed.url,
+						},
+						fimestamp: new Date(),
+						footer: {
+							test: 'Some footer text here',
+							icon_url: jsonParsed.url,
+						},
+					}
+
+					message.channel.send({
+						embed: Embed
+					});
+
+					client.on('message', (message1) => {
+						if ((filter === message1.author.id || message1.member.hasPermission("MANAGE_MESSAGES")) && message1.content.toLowerCase() === `${Prefix.toLowerCase()}stop`) {
+							run = false;
+						}else if(message1.author.Bot === false) {
+							const Embed = {
+								color: '#00ff00',
+								title: `Sorry you have to be the message author or the an admin`,
+								url: "",
+								author: {
+									Name: 'AnimeBot',
+									icon_url: "",
+									url: '',
+								},
+								description: ``,
+								thumbnail: "",
+								fields: [],
+								image: {
+									url: ""
+								},
+								fimestamp: new Date(),
+								footer: {
+									test: '',
+									icon_url: "",
+								},
+							}
+							message.channel.send({
+								embed: Embed
+							});
+						}
+					})
+				}
+			})
+				
+			}, args[1] * i * 1000);
+		}
 	} else if (commandName.toLowerCase() === 'autoawoo') {
 		if (!args[0]) args[0] = 3;
 		if (!args[1]) args[1] = 5;
 		var n = 0;
+		var run = true;
+		const filter = message.author.id;
 		for (i = 0; i < args[0]; i++) {
 			setTimeout(function(){
+				if (run === false) return;
 				n++;
 				request(`https://waifu.pics/api/sfw/awoo`, function(error, response, body) {
 				if (!error && response.statusCode == 200) {
@@ -959,6 +1110,37 @@ client.on('message', (message) => {
 					message.channel.send({
 						embed: Embed
 					});
+
+					client.on('message', (message1) => {
+						if ((filter === message1.author.id || message1.member.hasPermission("MANAGE_MESSAGES")) && message1.content.toLowerCase() === `${Prefix.toLowerCase()}stop`) {
+							run = false;
+						}else if(message1.author.Bot === false) {
+							const Embed = {
+								color: '#00ff00',
+								title: `Sorry you have to be the message author or the an admin`,
+								url: "",
+								author: {
+									Name: 'AnimeBot',
+									icon_url: "",
+									url: '',
+								},
+								description: ``,
+								thumbnail: "",
+								fields: [],
+								image: {
+									url: ""
+								},
+								fimestamp: new Date(),
+								footer: {
+									test: '',
+									icon_url: "",
+								},
+							}
+							message.channel.send({
+								embed: Embed
+							});
+						}
+					})
 				}
 			})
 				
@@ -968,8 +1150,11 @@ client.on('message', (message) => {
 		if (!args[0]) args[0] = 3;
 		if (!args[1]) args[1] = 5;
 		var n = 0;
+		var run = true;
+		const filter = message.author.id;
 		for (i = 0; i < args[0]; i++) {
 			setTimeout(function(){
+				if (run === false) return;
 				n++;
 				request(`https://waifu.pics/api/sfw/waifu`, function(error, response, body) {
 				if (!error && response.statusCode == 200) {
@@ -999,6 +1184,37 @@ client.on('message', (message) => {
 					message.channel.send({
 						embed: Embed
 					});
+
+					client.on('message', (message1) => {
+						if ((filter === message1.author.id || message1.member.hasPermission("MANAGE_MESSAGES")) && message1.content.toLowerCase() === `${Prefix.toLowerCase()}stop`) {
+							run = false;
+						}else if(message1.author.Bot === false) {
+							const Embed = {
+								color: '#00ff00',
+								title: `Sorry you have to be the message author or the an admin`,
+								url: "",
+								author: {
+									Name: 'AnimeBot',
+									icon_url: "",
+									url: '',
+								},
+								description: ``,
+								thumbnail: "",
+								fields: [],
+								image: {
+									url: ""
+								},
+								fimestamp: new Date(),
+								footer: {
+									test: '',
+									icon_url: "",
+								},
+							}
+							message.channel.send({
+								embed: Embed
+							});
+						}
+					})
 				}
 			})
 				
@@ -1009,8 +1225,11 @@ client.on('message', (message) => {
 		if (!args[1]) args[1] = 5;
 		if (message.channel.nsfw === true || message.guild === null) {
 			var n = 0;
+			var run = true;
+			const filter = message.author.id;
 			for (i = 0; i < args[0]; i++) {
 				setTimeout(function(){
+					if (run === false) return;
 					n++;
 					request(`https://waifu.pics/api/nsfw/waifu`, function(error, response, body) {
 					if (!error && response.statusCode == 200) {
@@ -1040,6 +1259,37 @@ client.on('message', (message) => {
 						message.channel.send({
 							embed: Embed
 						});
+
+						client.on('message', (message1) => {
+							if ((filter === message1.author.id || message1.member.hasPermission("MANAGE_MESSAGES")) && message1.content.toLowerCase() === `${Prefix.toLowerCase()}stop`) {
+								run = false;
+							}else if(message1.author.Bot === false) {
+								const Embed = {
+									color: '#00ff00',
+									title: `Sorry you have to be the message author or the an admin`,
+									url: "",
+									author: {
+										Name: 'AnimeBot',
+										icon_url: "",
+										url: '',
+									},
+									description: ``,
+									thumbnail: "",
+									fields: [],
+									image: {
+										url: ""
+									},
+									fimestamp: new Date(),
+									footer: {
+										test: '',
+										icon_url: "",
+									},
+								}
+								message.channel.send({
+									embed: Embed
+								});
+							}
+						})
 					}
 				})
 					
@@ -1369,6 +1619,8 @@ client.on('message', (message) => {
 				});
 			}
 		})
+	}else if(commandName.toLowerCase() === "stop") {
+
 	} else {
 		const Embed = {
 			color: '#00ff00',
