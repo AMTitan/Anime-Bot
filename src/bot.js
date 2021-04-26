0c28d9e2c3ed36248c3718654a71d00259c559f2
Arthur Melton <amtitan@icloud.com>
2021-04-26 16:07:48 -0400

    foxgirl

--- src/bot.js
+++ src/bot.js
@@ -17,7 +17,7 @@ const poster = new dbots.Poster({
         topgg: process.env.top
     },
     clientLibrary: 'discord.js'
-});
+})
 
 poster.startInterval();
 
@@ -79,6 +79,9 @@ client.on('message', (message) => {
 			}, {
 				name: `${Prefix}fact`,
 				value: "gets you a anime fact"
+			}, {
+				name: `${Prefix}fox [optinal nsfw]`,
+				value: "shows a random foxgirl img"
 			}, {
 				name: `${Prefix}hug`,
 				value: "gets you a hug gif or img"
@@ -405,6 +408,74 @@ client.on('message', (message) => {
 					const Embed = {
 						color: '#00ff00',
 						title: 'Neko',
+						url: "",
+						author: {
+							Name: 'AnimeBot',
+							icon_url: jsonParsed.url,
+							url: '',
+						},
+						description: ``,
+						thumbnail: jsonParsed.url,
+						fields: [],
+						image: {
+							url: jsonParsed.url,
+						},
+						fimestamp: new Date(),
+						footer: {
+							test: 'Some footer text here',
+							icon_url: jsonParsed.url,
+						},
+					}
+
+					message.channel.send({
+						embed: Embed
+					});
+				}
+			})
+		}
+	} else if (commandName.toLowerCase() === 'fox') {
+		if (args[0] && args[0].toLowerCase() === "nsfw") {
+			if (message.channel.nsfw === true || message.guild === null) {
+				request(`https://akaneko-api.herokuapp.com/api/foxgirl`, function(error, response, body) {
+					if (!error && response.statusCode == 200) {
+						var jsonParsed = JSON.parse(body);
+						const Embed = {
+							color: '#00ff00',
+							title: 'fox nsfw',
+							url: "",
+							author: {
+								Name: 'AnimeBot',
+								icon_url: jsonParsed.url,
+								url: '',
+							},
+							description: ``,
+							thumbnail: jsonParsed.url,
+							fields: [],
+							image: {
+								url: jsonParsed.url,
+							},
+							fimestamp: new Date(),
+							footer: {
+								test: 'Some footer text here',
+								icon_url: jsonParsed.url,
+							},
+						}
+
+						message.channel.send({
+							embed: Embed
+						});
+					}
+				})
+			} else {
+				message.channel.send("sorry but the channel is not marked as nsfw");
+			}
+		}else {
+			request(`https://akaneko-api.herokuapp.com/api/sfwfoxes`, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					var jsonParsed = JSON.parse(body);
+					const Embed = {
+						color: '#00ff00',
+						title: 'fox',
 						url: "",
 						author: {
 							Name: 'AnimeBot',
