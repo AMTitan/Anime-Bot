909f3e080782969b9d063590d7a80c5e91fe61a3
Arthur Melton <amtitan@icloud.com>
2021-04-26 17:44:08 -0400

    femdom

--- src/bot.js
+++ src/bot.js
@@ -1663,6 +1663,41 @@ client.on('message', (message) => {
 		} else {
 			message.channel.send("sorry but the channel is not marked as nsfw");
 		}
+	} else if (commandName.toLowerCase() === 'femdom') {
+		if (message.channel.nsfw === true || message.guild === null) {
+			request(`https://akaneko-api.herokuapp.com/api/femdom`, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					var jsonParsed = JSON.parse(body);
+					const Embed = {
+						color: '#00ff00',
+						title: 'femdom nsfw',
+						url: "",
+						author: {
+							Name: 'AnimeBot',
+							icon_url: jsonParsed.url,
+							url: '',
+						},
+						description: ``,
+						thumbnail: jsonParsed.url,
+						fields: [],
+						image: {
+							url: jsonParsed.url,
+						},
+						fimestamp: new Date(),
+						footer: {
+							test: 'Some footer text here',
+							icon_url: jsonParsed.url,
+						},
+					}
+
+					message.channel.send({
+						embed: Embed
+					});
+				}
+			})
+		} else {
+			message.channel.send("sorry but the channel is not marked as nsfw");
+		}
 	} else if (commandName.toLowerCase() === 'cumslut') {
 		if (message.channel.nsfw === true || message.guild === null) {
 			request(`https://akaneko-api.herokuapp.com/api/cumslut`, function(error, response, body) {
