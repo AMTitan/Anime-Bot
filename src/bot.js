df7c3d93fda1688aa25649b7ba6ec01cc75e0404
Arthur Melton <amtitan@icloud.com>
2021-04-26 19:13:54 -0400

    fixed

--- src/bot.js
+++ src/bot.js
@@ -518,36 +518,40 @@ client.on('message', (message) => {
 		}
 	} else if (commandName.toLowerCase() === 'random') {
 		if (args[0] === "gif") {
-			request(`https://nekos.life/api/v2/img/classic`, function(error, response, body) {
-				if (!error && response.statusCode == 200) {
-					var jsonParsed = JSON.parse(body);
-					const Embed = {
-						color: '#00ff00',
-						title: 'fox',
-						url: "",
-						author: {
-							Name: 'AnimeBot',
-							icon_url: jsonParsed.url,
-							url: '',
-						},
-						description: ``,
-						thumbnail: jsonParsed.url,
-						fields: [],
-						image: {
-							url: jsonParsed.url,
-						},
-						fimestamp: new Date(),
-						footer: {
-							test: 'Some footer text here',
-							icon_url: jsonParsed.url,
-						},
-					}
+			if (args[0] && args[0].toLowerCase() === "nsfw") {
+				request(`https://nekos.life/api/v2/img/classic`, function(error, response, body) {
+					if (!error && response.statusCode == 200) {
+						var jsonParsed = JSON.parse(body);
+						const Embed = {
+							color: '#00ff00',
+							title: 'fox',
+							url: "",
+							author: {
+								Name: 'AnimeBot',
+								icon_url: jsonParsed.url,
+								url: '',
+							},
+							description: ``,
+							thumbnail: jsonParsed.url,
+							fields: [],
+							image: {
+								url: jsonParsed.url,
+							},
+							fimestamp: new Date(),
+							footer: {
+								test: 'Some footer text here',
+								icon_url: jsonParsed.url,
+							},
+						}
 
-					message.channel.send({
-						embed: Embed
-					});
-				}
-			})
+						message.channel.send({
+							embed: Embed
+						});
+					}
+				})
+			} else {
+				message.channel.send("sorry but the channel is not marked as nsfw");
+			}
 		}else {
 			let array = ["pat", "hug", "waifu", "cry", "kiss", "slap", "smug", "punch"];
 			var item = array[Math.floor(Math.random() * array.length)];
