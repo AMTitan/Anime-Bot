3d69f43215da578748193da27ef27b988e9e6c09
Arthur Melton <amtitan@icloud.com>
2021-04-26 15:12:17 -0400

    better wallpaper

--- src/bot.js
+++ src/bot.js
@@ -119,7 +119,7 @@ client.on('message', (message) => {
 				name: `${Prefix}waifuquestions`,
 				value: "guess the waifu you get"
 			}, {
-				name: `${Prefix}wallpaper`,
+				name: `${Prefix}wallpaper [optinal nsfw]`,
 				value: "shows a random anime wallpaper (just a random one from the first 25 hot on the subreddit)"
 			}, {
 				name: `${Prefix}whatis {link to a img of a anime}`,
@@ -455,37 +455,73 @@ client.on('message', (message) => {
 			}
 		})
 	} else if (commandName.toLowerCase() === 'wallpaper') {
-		var aRandomNum = Math.floor((Math.random() * 24) + 2);
-		request('http://www.reddit.com/r/Animewallpaper.json?json', function(error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var jsonParsed = JSON.parse(body);
-				const Embed = {
-					color: '#00ff00',
-					title: jsonParsed.data.children[aRandomNum].data.title,
-					url: jsonParsed.data.children[aRandomNum].data.url,
-					author: {
-						Name: 'AnimeBot',
-						icon_url: "",
-						url: "",
-					},
-					description: ``,
-					thumbnail: "",
-					fields: [],
-					image: {
-						url: jsonParsed.data.children[aRandomNum].data.url_overridden_by_dest
-					},
-					fimestamp: new Date(),
-					footer: {
-						test: '',
-						icon_url: "",
-					},
-				}
+		if (args[0] && args[0].toLowerCase() === "nsfw") {
+			if (message.channel.nsfw === true || message.guild === null) {
+				request(`https://akaneko-api.herokuapp.com/api/nsfwwallpapers`, function(error, response, body) {
+					if (!error && response.statusCode == 200) {
+						var jsonParsed = JSON.parse(body);
+						const Embed = {
+							color: '#00ff00',
+							title: 'wallpaper nsfw',
+							url: "",
+							author: {
+								Name: 'AnimeBot',
+								icon_url: jsonParsed.url,
+								url: '',
+							},
+							description: ``,
+							thumbnail: jsonParsed.url,
+							fields: [],
+							image: {
+								url: jsonParsed.url,
+							},
+							fimestamp: new Date(),
+							footer: {
+								test: 'Some footer text here',
+								icon_url: jsonParsed.url,
+							},
+						}
 
-				message.channel.send({
-					embed: Embed
-				});
+						message.channel.send({
+							embed: Embed
+						});
+					}
+				})
+			} else {
+				message.channel.send("sorry but the channel is not marked as nsfw");
 			}
-		})
+		}else {
+			request(`https://akaneko-api.herokuapp.com/api/wallpapers`, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					var jsonParsed = JSON.parse(body);
+					const Embed = {
+						color: '#00ff00',
+						title: 'wallpaer',
+						url: "",
+						author: {
+							Name: 'AnimeBot',
+							icon_url: jsonParsed.url,
+							url: '',
+						},
+						description: ``,
+						thumbnail: jsonParsed.url,
+						fields: [],
+						image: {
+							url: jsonParsed.url,
+						},
+						fimestamp: new Date(),
+						footer: {
+							test: 'Some footer text here',
+							icon_url: jsonParsed.url,
+						},
+					}
+
+					message.channel.send({
+						embed: Embed
+					});
+				}
+			})
+		}
 	} else if (commandName.toLowerCase() === 'nsfw') {
 		if (message.channel.nsfw === true || message.guild === null) {
 			request('https://waifu.pics/api/nsfw/waifu', function(error, response, body) {
