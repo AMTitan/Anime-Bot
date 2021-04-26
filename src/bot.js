f9dbbbd6bff546b3432f28f909b8dbe03e61184a
Arthur Melton <amtitan@icloud.com>
2021-04-26 14:09:06 -0400

    nsfw cumslut

--- src/bot.js
+++ src/bot.js
@@ -1483,6 +1483,41 @@ client.on('message', (message) => {
 		} else {
 			message.channel.send("sorry but the channel is not marked as nsfw");
 		}
+	} else if (commandName.toLowerCase() === 'cumslut') {
+		if (message.channel.nsfw === true || message.guild === null) {
+			request(`https://akaneko-api.herokuapp.com/api/cumslut`, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					var jsonParsed = JSON.parse(body);
+					const Embed = {
+						color: '#00ff00',
+						title: 'cumslut nsfw',
+						url: jsonParsed.url,
+						author: {
+							Name: 'AnimeBot',
+							icon_url: jsonParsed.url,
+							url: '',
+						},
+						description: ``,
+						thumbnail: jsonParsed.url,
+						fields: [],
+						image: {
+							url: jsonParsed.url,
+						},
+						fimestamp: new Date(),
+						footer: {
+							test: 'Some footer text here',
+							icon_url: jsonParsed.url,
+						},
+					}
+
+					message.channel.send({
+						embed: Embed
+					});
+				}
+			})
+		} else {
+			message.channel.send("sorry but the channel is not marked as nsfw");
+		}
 	} else if (commandName.toLowerCase() === 'poke') {
 		request(`https://waifu.pics/api/sfw/poke`, function(error, response, body) {
 			if (!error && response.statusCode == 200) {
