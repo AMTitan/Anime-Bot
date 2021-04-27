c50680b4526208a70be94bc935878a0ca2c4ed3b
Arthur Melton <amtitan@icloud.com>
2021-04-27 12:33:10 -0400

    add a search cmd

--- src/bot.js
+++ src/bot.js
@@ -269,6 +269,9 @@ client.on('message', (message) => {
 				name: `${Prefix}school`,
 				value: "gets you a school girl img"
 			}, {
+				name: `${Prefix}search [tag]`,
+				value: "gets you a custom image img"
+			}, {
 				name: `${Prefix}tentacle`,
 				value: "gets you a tentacle img"
 			}, {
@@ -2334,6 +2337,43 @@ client.on('message', (message) => {
 						footer: {
 							test: 'Some footer text here',
 							icon_url: jsonParsed.url,
+						},
+					}
+
+					message.channel.send({
+						embed: Embed
+					});
+				}
+			})
+		} else {
+			message.channel.send("sorry but the channel is not marked as nsfw");
+		}
+	} else if (commandName.toLowerCase() === 'search') {
+		if (message.channel.nsfw === true || message.guild === null) {
+			request(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=${args[0]}&json=1`, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					var jsonParsed = JSON.parse(body);
+					jsonParsed = jsonParsed[Math.round(Math.random() * 100)];
+					console.log(jsonParsed.file_url);
+					const Embed = {
+						color: '#00ff00',
+						title: `${args[0]}`,
+						url: "",
+						author: {
+							Name: 'AnimeBot',
+							icon_url: jsonParsed.file_url,
+							url: '',
+						},
+						description: ``,
+						thumbnail: jsonParsed.file_url,
+						fields: [],
+						image: {
+							url: jsonParsed.file_url,
+						},
+						fimestamp: new Date(),
+						footer: {
+							test: 'Some footer text here',
+							icon_url: jsonParsed.file_url,
 						},
 					}
 
