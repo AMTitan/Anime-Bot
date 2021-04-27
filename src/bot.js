fe51a5a635c058c3b2dbd4214b2883ea34a814c3
Arthur Melton <amtitan@icloud.com>
2021-04-27 14:12:30 -0400

    seach say if it failed

--- src/bot.js
+++ src/bot.js
@@ -2352,10 +2352,93 @@ client.on('message', (message) => {
 		if (message.channel.nsfw === true || message.guild === null) {
 			request(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=${args.join(" ")}&json=1`, function(error, response, body) {
 				if (!error && response.statusCode == 200) {
-					if (!body) return;
+					var shouldIReturn = false;
+					if (!body) {
+						const Embed = {
+							color: '#00ff00',
+							title: `Sorry but I could not find a img with that tag`,
+							url: "",
+							author: {
+								Name: 'AnimeBot',
+								icon_url: "",
+								url: '',
+							},
+							description: ``,
+							thumbnail: "",
+							fields: [],
+							image: {
+								url: ""
+							},
+							fimestamp: new Date(),
+							footer: {
+								test: '',
+								icon_url: "",
+							},
+						}
+						message.channel.send({
+							embed: Embed
+						});
+						var shouldIReturn = true;
+					}
+					if (shouldIReturn) return;
 					var jsonParsed = JSON.parse(body);
+					if (!jsonParsed) {
+						const Embed = {
+							color: '#00ff00',
+							title: `Sorry but I could not find a img with that tag`,
+							url: "",
+							author: {
+								Name: 'AnimeBot',
+								icon_url: "",
+								url: '',
+							},
+							description: ``,
+							thumbnail: "",
+							fields: [],
+							image: {
+								url: ""
+							},
+							fimestamp: new Date(),
+							footer: {
+								test: '',
+								icon_url: "",
+							},
+						}
+						message.channel.send({
+							embed: Embed
+						});
+						var shouldIReturn = true;
+					}
+					if (shouldIReturn) return;
 					jsonParsed = jsonParsed[Math.round(Math.random() * jsonParsed.length)];
-					if (!jsonParsed.file_url) return;
+					if (!jsonParsed.file_url) {
+						const Embed = {
+							color: '#00ff00',
+							title: `Sorry but I could not find a img with that tag`,
+							url: "",
+							author: {
+								Name: 'AnimeBot',
+								icon_url: "",
+								url: '',
+							},
+							description: ``,
+							thumbnail: "",
+							fields: [],
+							image: {
+								url: ""
+							},
+							fimestamp: new Date(),
+							footer: {
+								test: '',
+								icon_url: "",
+							},
+						}
+						message.channel.send({
+							embed: Embed
+						});
+						var shouldIReturn = true;
+					}
+					if (shouldIReturn) return;
 					const Embed = {
 						color: '#00ff00',
 						title: `${args.join(", ")} img`,
