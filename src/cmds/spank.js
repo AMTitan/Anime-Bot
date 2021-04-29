ec1232963f9f988c0afe50ca4c48de3e7e6db59e
Arthur Melton <amtitan@icloud.com>
2021-04-28 20:19:24 -0400

    spank

--- src/cmds/spank.js
+++ src/cmds/spank.js
@@ -1,6 +1,6 @@
 module.exports = function(Prefix, message, commandName, args, request, client) {
 	if (message.channel.nsfw === true || message.guild === null) {
-		request(`https://waifu.pics/api/spank`, function(error, response, body) {
+		request(`https://nekos.life/api/v2/img/spank`, function(error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var jsonParsed = JSON.parse(body);
 				const Embed = {
