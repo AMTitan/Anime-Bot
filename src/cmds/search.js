3717d922979aafec0f012ad24b6a9e99cd0440c5
Arthur Melton <amtitan@icloud.com>
2021-04-30 08:48:02 -0400

    idk what I did

--- src/cmds/search.js
+++ src/cmds/search.js
@@ -147,30 +147,30 @@ module.exports = function(Prefix, message, commandName, args, request, client) {
 			})
 		} else {
 			const Embed = {
-			color: '#00ff00',
-			title: 'sorry but the channel is not marked as nsfw (to make it nsfw go to the channel settings and make nsfw on)',
-			url: "",
-			author: {
-				Name: 'AnimeBot',
-				icon_url: "",
-				url: '',
-			},
-			description: ``,
-			thumbnail: "",
-			fields: [],
-			image: {
+				color: '#00ff00',
+				title: 'sorry but the channel is not marked as nsfw (to make it nsfw go to the channel settings and make nsfw on)',
 				url: "",
-			},
-			fimestamp: new Date(),
-			footer: {
-				test: 'Some footer text here',
-				icon_url: "",
-			},
-		}
+				author: {
+					Name: 'AnimeBot',
+					icon_url: "",
+					url: '',
+				},
+				description: ``,
+				thumbnail: "",
+				fields: [],
+				image: {
+					url: "",
+				},
+				fimestamp: new Date(),
+				footer: {
+					test: 'Some footer text here',
+					icon_url: "",
+				},
+			}
 
-		message.channel.send({
-			embed: Embed
-		});
+			message.channel.send({
+				embed: Embed
+			});
 		}
 	}else if (args[0] === "gif") {
 		args.shift();
