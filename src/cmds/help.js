e11f1dc65c753de52a57db3f0efebc9518c2a9a2
Arthur Melton <amtitan@icloud.com>
2021-05-04 14:41:21 -0400

    cmds.json

--- src/cmds/help.js
+++ src/cmds/help.js
@@ -1,4 +1,8 @@
 module.exports = function(Prefix, message) {
+	var cmds = require("../cmds.json");
+	cmds = JSON.stringify(cmds);
+	cmds = cmds.replaceAll("${Prefix}", Prefix);
+	cmds = JSON.parse(cmds);
 	const Embed = {
 		color: '#00ff00',
 		title: `Help - Other`,
@@ -10,88 +14,7 @@ module.exports = function(Prefix, message) {
 		},
 		description: ``,
 		thumbnail: "",
-		fields: [{
-			name: `EXAMPLE`,
-			value: `${Prefix}neko [optinal nsfw]`
-		}, {
-			name: `${Prefix}issue (the issue)`,
-			value: "if you find a issue"
-		}, {
-			name: `${Prefix}improve (the improvement)`,
-			value: "if you want something to be improved"
-		}, {
-			name: `${Prefix}awoo`,
-			value: "gets you a awoo gif or img"
-		}, {
-			name: `${Prefix}baka [optinal @ person]`,
-			value: "gets you a baka gif"
-		},  {
-			name: `${Prefix}blush [optinal @ person]`,
-			value: "gets you a blush gif or img"
-		}, {
-			name: `${Prefix}cuddle [optinal @ person]`,
-			value: "gets you a cuddle gif or img"
-		}, {
-			name: `${Prefix}fact`,
-			value: "gets you a anime fact"
-		}, {
-			name: `${Prefix}fox [optinal nsfw]`,
-			value: "shows a random foxgirl img"
-		}, {
-			name: `${Prefix}hug [optinal @ person]`,
-			value: "gets you a hug gif or img"
-		}, {
-			name: `${Prefix}invite`,
-			value: "gets you the invite to invite the bot to your server"
-		}, {
-			name: `${Prefix}kiss [optinal @ person]`,
-			value: "gets you a kiss gif or img"
-		}, {
-			name: `${Prefix}neko [optinal nsfw or gif]`,
-			value: "shows a random neko img or gif"
-		}, {
-			name: `${Prefix}pat [optinal @ person]`,
-			value: "gets you a pat gif or img"
-		}, {
-			name: `${Prefix}poke [optinal @ person]`,
-			value: "gets you a poke gif"
-		}, {
-			name: `${Prefix}quote`,
-			value: "shows a random anime quote"
-		}, {
-			name: `${Prefix}quotequestions`,
-			value: "guess who said a quote"
-		}, {
-			name: `${Prefix}random [optinal gif]`,
-			value: "shows a random anime img"
-		}, {
-			name: `${Prefix}search gif [tags (you can do more than one) example : "${Prefix}search gif zero two"]`,
-			value: "gets you a custom gif"
-		}, {
-			name: `${Prefix}server`,
-			value: "gets you the main anime bot server"
-		}, {
-			name: `${Prefix}show`,
-			value: "gets you a random "
-		}, {
-			name: `${Prefix}stats`,
-			value: "gets you the bot stats"
-		}, {
-			name: `${Prefix}stop`,
-			value: "stops a auto anything but you have to be the msg author or able to delete msges"
-		}, {
-			name: `${Prefix}waifu`,
-			value: "gets you a waifu"
-		}, {
-			name: `${Prefix}waifuquestions`,
-			value: "guess the waifu you get"
-		}, {
-			name: `${Prefix}wallpaper [optinal nsfw]`,
-			value: "shows a random anime wallpaper (just a random one from the first 25 hot on the subreddit)"
-		}, {
-			name: `${Prefix}whatis {link to a img of a anime}`,
-			value: "tries to find what anime it was in"
-		},],
+		fields: cmds.other,
 		image: {
 			url: ""
 		},
@@ -113,31 +36,7 @@ module.exports = function(Prefix, message) {
 		},
 		description: ``,
 		thumbnail: "",
-		fields: [{
-			name: `EXAMPLE`,
-			value: `${Prefix}autonekonsfw`
-		},  {
-			name: `${Prefix}autoawoo (number of times to run nsfw - default 10) (delay in secs - defualt 5)`,
-			value: "dose awoo a set number of times"
-		},  {
-			name: `${Prefix}autoblowjob (number of times to run nsfw - default 10) (delay in secs - defualt 5)`,
-			value: "dose awoo a set number of times"
-		},  {
-			name: `${Prefix}automaid (number of times to run nsfw - default 10) (delay in secs - defualt 5)`,
-			value: "dose maid a set number of times"
-		},  {
-			name: `${Prefix}autoneko (number of times to run nsfw - default 10) (delay in secs - defualt 5)`,
-			value: "dose neko a set number of times"
-		},   {
-			name: `${Prefix}autonekonsfw (number of times to run nsfw - default 10) (delay in secs - defualt 5)`,
-			value: "dose neko nsfw a set number of times"
-		},  {
-			name: `${Prefix}autonsfw (number of times to run nsfw - default 10) (delay in secs - defualt 5)`,
-			value: "dose nsfw a set number of times"
-		},  {
-			name: `${Prefix}autowaifu (number of times to run nsfw - default 10) (delay in secs - defualt 5)`,
-			value: "dose waifu a set number of times"
-		},],
+		fields: cmds.auto,
 		image: {
 			url: ""
 		},
@@ -159,88 +58,29 @@ module.exports = function(Prefix, message) {
 		},
 		description: ``,
 		thumbnail: "",
-		fields: [ {
-			name: `EXAMPLE`,
-			value: `${Prefix}panties nsfw`
-		},{
-			name: `${Prefix}anal`,
-			value: `gets you a anal gif`
-		},  {
-			name: `${Prefix}ass`,
-			value: "gets you a ass img"
-		},  {
-			name: `${Prefix}bdsm`,
-			value: "gets you a bdsm (you like it or you dont) gif"
-		}, {
-			name: `${Prefix}blowjob`,
-			value: "gets you a blowjob gif"
-		}, {
-			name: `${Prefix}boobs`,
-			value: "gets you a boobs gif"
-		}, {
-			name: `${Prefix}cumslut`,
-			value: "gets you a cumslut gif"
-		}, {
-			name: `${Prefix}feet`,
-			value: "gets you a feet img"
-		}, {
-			name: `${Prefix}femdom`,
-			value: "gets you a female dominance img"
-		}, {
-			name: `${Prefix}gif`,
-			value: "gets you a nsfw gif!"
-		}, {
-			name: `${Prefix}glasses`,
-			value: "gets you a glasses img"
-		}, {
-			name: `${Prefix}maid`,
-			value: "gets you a maid img"
-		}, {
-			name: `${Prefix}masturbation`,
-			value: "gets you a masturbation img"
-		}, {
-			name: `${Prefix}nsfw`,
-			value: "shows random nsfw anime img"
-		}, {
-			name: `${Prefix}orgy`,
-			value: "gets you a orgy (group) img"
-		}, {
-			name: `${Prefix}panties`,
-			value: "gets you a panties img"
-		}, {
-			name: `${Prefix}pussy [optinal gif]`,
-			value: "gets you a pussy img"
-		}, {
-			name: `${Prefix}school`,
-			value: "gets you a school girl img"
-		}, {
-			name: `${Prefix}search img [tags (you can do more than one) example : "${Prefix}search img school_girl"]`,
-			value: "gets you a custom image"
-		},  {
-			name: `${Prefix}solo [optinal gif]`,
-			value: "gets you a solo gif or img"
-		},  {
-			name: `${Prefix}spank`,
-			value: "gets you a spank gif"
-		},  {
-			name: `${Prefix}tentacle`,
-			value: "gets you a tentacle img"
-		}, {
-			name: `${Prefix}thighs`,
-			value: "gets you a thighs img"
-		}, {
-			name: `${Prefix}uniform`,
-			value: "gets you a uniform img"
-		}, {
-			name: `${Prefix}whatisnsfw`,
-			value: "runs nsfw then dose a whatis on it (gets a random hentai)"
-		},{
-			name: `${Prefix}yuri`,
-			value: "gets you a girl on girl"
-		}, {
-			name: `${Prefix}yaoi`,
-			value: "gets you a guy on guy"
-		},],
+		fields: cmds.nsfw,
+		image: {
+			url: ""
+		},
+		fimestamp: new Date(),
+		footer: {
+			test: '',
+			icon_url: "",
+		},
+	}
+
+	const Embedfive= {
+		color: '#00ff00',
+		title: `Help - Nsfw (channel has to be marked nsfw)`,
+		url: "",
+		author: {
+			Name: 'AnimeBot',
+			icon_url: "",
+			url: '',
+		},
+		description: ``,
+		thumbnail: "",
+		fields: cmds.sfw,
 		image: {
 			url: ""
 		},
@@ -293,6 +133,10 @@ module.exports = function(Prefix, message) {
 
 	message.author.send({
 		embed: Embedthree
+	})
+
+	message.author.send({
+		embed: Embedfive
 	})
 	.catch(() => message.channel.send({
 		embed: Embedthree
