9e40af788f7cec311ef0db0886ada7c7d920729c
Arthur Melton <amtitan@icloud.com>
2021-05-31 00:04:08 -0400

    masturbation gif and futanari

--- src/cmds/masturbation.js
+++ src/cmds/masturbation.js
@@ -1,35 +1,69 @@
 module.exports = function(Prefix, message, commandName, args, request, client) {
 	if (message.channel.nsfw === true || message.guild === null) {
-		request(`https://akaneko-api.herokuapp.com/api/masturbation`, function(error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var jsonParsed = JSON.parse(body);
-				const Embed = {
-					color: '#00ff00',
-					title: 'masturbation nsfw',
-					url: "",
-					author: {
-						Name: 'AnimeBot',
-						icon_url: jsonParsed.url,
-						url: '',
-					},
-					description: ``,
-					thumbnail: jsonParsed.url,
-					fields: [],
-					image: {
-						url: jsonParsed.url,
-					},
-					fimestamp: new Date(),
-					footer: {
-						test: 'Some footer text here',
-						icon_url: jsonParsed.url,
-					},
-				}
+		if (args[0] === "gif") {
+			request(`https://nekos.life/api/v2/img/pwankg`, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					var jsonParsed = JSON.parse(body);
+					const Embed = {
+						color: '#00ff00',
+						title: 'Masturbation gif nsfw',
+						url: "",
+						author: {
+							Name: 'AnimeBot',
+							icon_url: jsonParsed.url,
+							url: '',
+						},
+						description: ``,
+						thumbnail: jsonParsed.url,
+						fields: [],
+						image: {
+							url: jsonParsed.url,
+						},
+						fimestamp: new Date(),
+						footer: {
+							test: 'Some footer text here',
+							icon_url: jsonParsed.url,
+						},
+					}
 
-				message.channel.send({
-					embed: Embed
-				});
-			}
-		})
+					message.channel.send({
+						embed: Embed
+					});
+				}
+			})
+		}
+		else {
+			request(`https://akaneko-api.herokuapp.com/api/masturbation`, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					var jsonParsed = JSON.parse(body);
+					const Embed = {
+						color: '#00ff00',
+						title: 'masturbation nsfw',
+						url: "",
+						author: {
+							Name: 'AnimeBot',
+							icon_url: jsonParsed.url,
+							url: '',
+						},
+						description: ``,
+						thumbnail: jsonParsed.url,
+						fields: [],
+						image: {
+							url: jsonParsed.url,
+						},
+						fimestamp: new Date(),
+						footer: {
+							test: 'Some footer text here',
+							icon_url: jsonParsed.url,
+						},
+					}
+
+					message.channel.send({
+						embed: Embed
+					});
+				}
+			})
+		}
 	} else {
 		const Embed = {
 			color: '#00ff00',
