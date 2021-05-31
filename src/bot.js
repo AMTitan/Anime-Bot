bdfa99eb2c910658036079034bc5798725a1f7ad
Arthur Melton <amtitan@icloud.com>
2021-05-30 23:43:08 -0400

    keta

--- src/bot.js
+++ src/bot.js
@@ -174,6 +174,7 @@ client.on('message', (message) => {
 	else if (commandName.toLowerCase() === 'erofeet') require("./cmds/erofeet.js")(Prefix, message, commandName, args, request, client);
 	else if (commandName.toLowerCase() === 'erok') require("./cmds/erok.js")(Prefix, message, commandName, args, request, client);
 	else if (commandName.toLowerCase() === 'hololewd') require("./cmds/hololewd.js")(Prefix, message, commandName, args, request, client);
+	else if (commandName.toLowerCase() === 'keta') require("./cmds/keta.js")(Prefix, message, commandName, args, request, client);
 	else require("./cmds/else.js")(Prefix, message, commandName, args, request, client);
 });
 
