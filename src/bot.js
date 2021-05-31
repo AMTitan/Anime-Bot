f4fc969aeecfee54c9e836e129b29b787a29a597
Arthur Melton <amtitan@icloud.com>
2021-05-31 01:14:02 -0400

    eropantyhose

--- src/bot.js
+++ src/bot.js
@@ -191,6 +191,7 @@ client.on('message', (message) => {
 	else if (commandName.toLowerCase() === 'ahegaoavatar') require("./cmds/ahegao_avatar.js")(Prefix, message, commandName, args, request, client);
 	else if (commandName.toLowerCase() === 'cosplay') require("./cmds/cosplay.js")(Prefix, message, commandName, args, request, client);
 	else if (commandName.toLowerCase() === 'pantyhose') require("./cmds/pantyhose.js")(Prefix, message, commandName, args, request, client);
+	else if (commandName.toLowerCase() === 'eropantyhose') require("./cmds/eropantyhose.js")(Prefix, message, commandName, args, request, client);
 	else require("./cmds/else.js")(Prefix, message, commandName, args, request, client);
 });
 
