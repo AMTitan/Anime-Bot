12833fa00b3eeb1b05882b67b9360300ebbc1c64
Arthur Melton <amtitan@icloud.com>
2021-05-30 23:38:43 -0400

    erok

--- src/bot.js
+++ src/bot.js
@@ -172,6 +172,7 @@ client.on('message', (message) => {
 	else if (commandName.toLowerCase() === 'cum') require("./cmds/cum.js")(Prefix, message, commandName, args, request, client);
 	else if (commandName.toLowerCase() === 'ero') require("./cmds/ero.js")(Prefix, message, commandName, args, request, client);
 	else if (commandName.toLowerCase() === 'erofeet') require("./cmds/erofeet.js")(Prefix, message, commandName, args, request, client);
+	else if (commandName.toLowerCase() === 'erok') require("./cmds/erok.js")(Prefix, message, commandName, args, request, client);
 	else require("./cmds/else.js")(Prefix, message, commandName, args, request, client);
 });
 
