757de244e697a9963671fae7e693e619190ebbae
Arthur Melton <amtitan@icloud.com>
2021-05-31 08:57:33 -0400

    update api feetjob

--- src/cmds/feetjob.js
+++ src/cmds/feetjob.js
@@ -1,6 +1,6 @@
 module.exports = function(Prefix, message, commandName, args, request, client) {
 	if (message.channel.nsfw === true || message.guild === null) {
-		request(`https://nekos.life/api/v2/img/feetg`, function(error, response, body) {
+		request(`https://api.nekos.dev/api/v3/images/nsfw/gif/feet/`, function(error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var jsonParsed = JSON.parse(body);
 				const Embed = {
@@ -9,19 +9,19 @@ module.exports = function(Prefix, message, commandName, args, request, client) {
 					url: "",
 					author: {
 						Name: 'AnimeBot',
-						icon_url: jsonParsed.url,
+						icon_url: jsonParsed.data.response.url,
 						url: '',
 					},
 					description: ``,
-					thumbnail: jsonParsed.url,
+					thumbnail: jsonParsed.data.response.url,
 					fields: [],
 					image: {
-						url: jsonParsed.url,
+						url: jsonParsed.data.response.url,
 					},
 					fimestamp: new Date(),
 					footer: {
 						test: 'Some footer text here',
-						icon_url: jsonParsed.url,
+						icon_url: jsonParsed.data.response.url,
 					},
 				}
 
