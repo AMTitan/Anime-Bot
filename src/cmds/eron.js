0a832afc0d457838f1562c47d13ce78efbac0aeb
Arthur Melton <amtitan@icloud.com>
2021-05-31 08:51:07 -0400

    update api ero neko

--- src/cmds/eron.js
+++ src/cmds/eron.js
@@ -1,6 +1,6 @@
 module.exports = function(Prefix, message, commandName, args, request, client) {
 	if (message.channel.nsfw === true || message.guild === null) {
-		request(`https://nekos.life/api/v2/img/eron`, function(error, response, body) {
+		request(`https://api.nekos.dev/api/v3/images/nsfw/img/neko_ero/`, function(error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var jsonParsed = JSON.parse(body);
 				const Embed = {
