a3288dfd573cba79c233cfd56693002bf83e0770
Arthur Melton <amtitan@icloud.com>
2021-06-08 16:26:08 -0400

    anal has imgs now

--- src/cmds/anal.js
+++ src/cmds/anal.js
@@ -1,6 +1,7 @@
 module.exports = function(Prefix, message, commandName, args, request, client) {
     if (message.channel.nsfw === true || message.guild === null) {
-        request(`https://api.nekos.dev/api/v3/images/nsfw/gif/anal/`, function(error, response, body) {
+        if (args[0] === "gif") {
+            request(`https://api.nekos.dev/api/v3/images/nsfw/gif/anal/`, function(error, response, body) {
             if (!error && response.statusCode == 200) {
                 var jsonParsed = JSON.parse(body);
                 const Embed = {
@@ -30,6 +31,39 @@ module.exports = function(Prefix, message, commandName, args, request, client) {
                 });
             }
         })
+        }
+        else {
+            request(`https://api.nekos.dev/api/v3/images/nsfw/gif/anal/`, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var jsonParsed = JSON.parse(body);
+                    const Embed = {
+                        color: '#00ff00',
+                        title: 'anal nsfw',
+                        url: "",
+                        author: {
+                            Name: 'AnimeBot',
+                            icon_url: jsonParsed.data.response.url.split(" ").join("%20"),
+                            url: '',
+                        },
+                        description: ``,
+                        thumbnail: jsonParsed.data.response.url.split(" ").join("%20"),
+                        fields: [],
+                        image: {
+                            url: jsonParsed.data.response.url.split(" ").join("%20"),
+                        },
+                        fimestamp: new Date(),
+                        footer: {
+                            test: 'Some footer text here',
+                            icon_url: jsonParsed.data.response.url.split(" ").join("%20"),
+                        },
+                    }
+
+                    message.channel.send({
+                        embed: Embed
+                    });
+                }
+            })
+        }
     } else {
         const Embed = {
             color: '#00ff00',
