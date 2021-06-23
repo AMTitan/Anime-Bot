524fe4207f0a1b3c7bf9a035006aba3d5631b5cb
Arthur Melton <amtitan@icloud.com>
2021-06-22 22:32:12 -0400

    pass what is needed

--- src/cmds/guild.js
+++ src/cmds/guild.js
@@ -1,4 +1,4 @@
-module.exports = function(Prefix, message, commandName, args, request, client) {
+module.exports = function(Prefix, message, commandName, args, request, client, owner, Levels) {
     if (message.author.id === owner) {
         message.channel.send(message.client.guilds.cache.get(args[0]).toString());
     }
