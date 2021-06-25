2b19bcf63ed3297e7fa7f0d5e826dccde6b290ab
Arthur Melton <amtitan@icloud.com>
2021-06-25 11:07:44 -0400

    genshin

--- src/cmds/genshin.js
+++ src/cmds/genshin.js
@@ -1,38 +1,33 @@
 module.exports = function(Prefix, message, commandName, args, request, client) {
     if (message.channel.nsfw === true || message.guild === null) {
-        request(`https://nekobot.xyz/api/image?type=food`, function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var jsonParsed = JSON.parse(body);
-                const lewd = require("ereshkigal");
-                const client = new lewd.Client();
+        const lewd = require("ereshkigal");
+        const client = new lewd.Client();
 
-                client.genshin().then((data) => {
-                    const Embed = {
-                        color: '#00ff00',
-                        title: commandName,
-                        url: "",
-                        author: {
-                            Name: 'AnimeBot',
-                            icon_url: data.split("\n")[Math.round(Math.random() * data.split("\n").length-1)],
-                            url: '',
-                        },
-                        description: ``,
-                        thumbnail: data.split("\n")[Math.round(Math.random() * data.split("\n").length-1)],
-                        fields: [],
-                        image: {
-                            url: data.split("\n")[Math.round(Math.random() * data.split("\n").length-1)],
-                        },
-                        footer: {
-                            test: 'Some footer text here',
-                            icon_url: data.split("\n")[Math.round(Math.random() * data.split("\n").length-1)],
-                        },
-                    }
-                    message.channel.send({
-                        embed: Embed
-                    });
-                });
+        client.genshin().then((data) => {
+            const Embed = {
+                color: '#00ff00',
+                title: commandName,
+                url: "",
+                author: {
+                    Name: 'AnimeBot',
+                    icon_url: data.split("\n")[Math.round(Math.random() * data.split("\n").length-1)],
+                    url: '',
+                },
+                description: ``,
+                thumbnail: data.split("\n")[Math.round(Math.random() * data.split("\n").length-1)],
+                fields: [],
+                image: {
+                    url: data.split("\n")[Math.round(Math.random() * data.split("\n").length-1)],
+                },
+                footer: {
+                    test: 'Some footer text here',
+                    icon_url: data.split("\n")[Math.round(Math.random() * data.split("\n").length-1)],
+                },
             }
-        })
+            message.channel.send({
+                embed: Embed
+            });
+        });
     } else {
         const Embed = {
             color: '#00ff00',
