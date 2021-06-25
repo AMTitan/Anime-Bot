ff54c6ed3d87cdc7ae1eeea59872ab89340524c6
Arthur Melton <amtitan@icloud.com>
2021-06-25 11:35:28 -0400

    dota and genshin better

--- src/cmds/genshin.js
+++ src/cmds/genshin.js
@@ -1,33 +1,41 @@
 module.exports = function(Prefix, message, commandName, args, request, client) {
     if (message.channel.nsfw === true || message.guild === null) {
-        const lewd = require("ereshkigal");
-        const client = new lewd.Client();
 
-        client.genshin().then((data) => {
-            const Embed = {
-                color: '#00ff00',
-                title: commandName,
-                url: "",
-                author: {
-                    Name: 'AnimeBot',
-                    icon_url: data.split("\n")[Math.round(Math.random() * data.split("\n").length-1)],
-                    url: '',
-                },
-                description: ``,
-                thumbnail: data.split("\n")[Math.round(Math.random() * data.split("\n").length-1)],
-                fields: [],
-                image: {
-                    url: data.split("\n")[Math.round(Math.random() * data.split("\n").length-1)],
-                },
-                footer: {
-                    test: 'Some footer text here',
-                    icon_url: data.split("\n")[Math.round(Math.random() * data.split("\n").length-1)],
-                },
+        request(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=-loli -asian -3d -photo_(medium) rating:explicit genshin_impact -loli&json=1`, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var jsonParsed = JSON.parse(body)[Math.round(Math.random() * (JSON.parse(body).length - 1))];
+                const Embed = {
+                    color: '#00ff00',
+                    title: commandName,
+                    url: "",
+                    author: {
+                        Name: 'AnimeBot',
+                        icon_url: jsonParsed.file_url,
+                        url: '',
+                    },
+                    //description: `(I need to fix this but if you get no img do do this again) If this is not very good it is bc I dont have this cmd coded but to see my offical cmds you can do ` + "`" + `${Prefix}help` + "`" + ` But if you really want this command you can ` + "`" + `${Prefix}improve ${commandName} ${args.join(" ")} (reason)` + "`\n\n**Tags: **" + "```" + jsonParsed.tags + "```",
+                    thumbnail: jsonParsed.file_url,
+                    fields: [],
+                    image: {
+                        url: jsonParsed.file_url,
+                    },
+                    footer: {
+                        test: 'Some footer text here',
+                        icon_url: jsonParsed.file_url,
+                    },
+                }
+
+                message.channel.send({
+                    embed: Embed
+                });
+
+                if (!jsonParsed.file_url.endsWith(".jpg") && !jsonParsed.file_url.endsWith(".jpeg") && !jsonParsed.file_url.endsWith(".JPG") && !jsonParsed.file_url.endsWith(".JPEG") && !jsonParsed.file_url.endsWith(".png") && !jsonParsed.file_url.endsWith(".PNG") && !jsonParsed.file_url.endsWith(".gif") && !jsonParsed.file_url.endsWith(".gifv")) {
+                    message.channel.send(jsonParsed.file_url);
+                }
             }
-            message.channel.send({
-                embed: Embed
-            });
-        });
+        })
+        
+        
     } else {
         const Embed = {
             color: '#00ff00',
