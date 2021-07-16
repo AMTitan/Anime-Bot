a577e5cae8071cbc55f39731c5e3acf9ede5054c
Arthur Melton <amtitan@icloud.com>
2021-07-16 12:50:20 -0400

    (now works)

--- src/cmds/boobs.js
+++ src/cmds/boobs.js
@@ -1,33 +1,31 @@
 module.exports = function(message, commandName, args, client) {
     if (message.channel.nsfw === true || message.guild === null) {
-        request(`https://api.nekos.dev/api/v3/images/nsfw/gif/tits/`).then(res => res.text()).then(body => {
-            if (!error && response.statusCode === 200) {
-                var jsonParsed = JSON.parse(body);
-                const Embed = {
-                    color: '#00ff00',
-                    title: 'boobs nsfw',
-                    url: "",
-                    author: {
-                        Name: 'AnimeBot',
-                        icon_url: jsonParsed.data.response.url.split(" ").join("%20"),
-                        url: '',
-                    },
-                    description: ``,
-                    thumbnail: jsonParsed.data.response.url.split(" ").join("%20"),
-                    fields: [],
-                    image: {
-                        url: jsonParsed.data.response.url.split(" ").join("%20"),
-                    },
-                    footer: {
-                        test: 'Some footer text here',
-                        icon_url: jsonParsed.data.response.url.split(" ").join("%20"),
-                    },
-                }
-
-                message.channel.send({
-                    embed: Embed
-                });
+        client.request(`https://api.nekos.dev/api/v3/images/nsfw/gif/tits/`).then(res => res.text()).then(body => {
+            var jsonParsed = JSON.parse(body);
+            const Embed = {
+                color: '#00ff00',
+                title: 'boobs nsfw',
+                url: "",
+                author: {
+                    Name: 'AnimeBot',
+                    icon_url: jsonParsed.data.response.url.split(" ").join("%20"),
+                    url: '',
+                },
+                description: ``,
+                thumbnail: jsonParsed.data.response.url.split(" ").join("%20"),
+                fields: [],
+                image: {
+                    url: jsonParsed.data.response.url.split(" ").join("%20"),
+                },
+                footer: {
+                    test: 'Some footer text here',
+                    icon_url: jsonParsed.data.response.url.split(" ").join("%20"),
+                },
             }
+
+            message.channel.send({
+                embed: Embed
+            });
         })
     } else {
         const Embed = {
--- src/cmds/wallpaper.js
+++ src/cmds/wallpaper.js
@@ -1,34 +1,32 @@
 module.exports = function(message, commandName, args, client) {
     if (args[0] && args[0].toLowerCase() === "nsfw") {
         if (message.channel.nsfw === true || message.guild === null) {
-            request(`https://akaneko-api.herokuapp.com/api/nsfwwallpapers`).then(res => res.text()).then(body => {
-                if (!error && response.statusCode === 200) {
-                    var jsonParsed = JSON.parse(body);
-                    const Embed = {
-                        color: '#00ff00',
-                        title: 'wallpaper nsfw',
-                        url: "",
-                        author: {
-                            Name: 'AnimeBot',
-                            icon_url: jsonParsed.url,
-                            url: '',
-                        },
-                        description: ``,
-                        thumbnail: jsonParsed.url,
-                        fields: [],
-                        image: {
-                            url: jsonParsed.url,
-                        },
-                        footer: {
-                            test: 'Some footer text here',
-                            icon_url: jsonParsed.url,
-                        },
-                    }
-
-                    message.channel.send({
-                        embed: Embed
-                    });
+            client.request(`https://akaneko-api.herokuapp.com/api/nsfwwallpapers`).then(res => res.text()).then(body => {
+                var jsonParsed = JSON.parse(body);
+                const Embed = {
+                    color: '#00ff00',
+                    title: 'wallpaper nsfw',
+                    url: "",
+                    author: {
+                        Name: 'AnimeBot',
+                        icon_url: jsonParsed.url,
+                        url: '',
+                    },
+                    description: ``,
+                    thumbnail: jsonParsed.url,
+                    fields: [],
+                    image: {
+                        url: jsonParsed.url,
+                    },
+                    footer: {
+                        test: 'Some footer text here',
+                        icon_url: jsonParsed.url,
+                    },
                 }
+
+                message.channel.send({
+                    embed: Embed
+                });
             })
         } else {
             const Embed = {
